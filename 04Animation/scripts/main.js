08b29b5c86b314018524e5b8a5416d877da2f3ec
Suuks <61313109+Suuks@users.noreply.github.com>
2021-05-15 18:14:30 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -4,7 +4,15 @@
 var rotationSpeed = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
-
+//probeersel met easing of whatever//
+//var defaultTweenSettings = {
+//  duration: 1000,
+//  delay: 0,
+//  endDelay: 0,
+//  easing: 'easeOutElastic(1, .5)',
+ // round: 0
+//};
+//https://github.com/juliangarnier/anime/blob/master/lib/anime.js
 
 function spin(){
 	myOtherBox.object3D.rotation.x +=(rotationSpeed);
