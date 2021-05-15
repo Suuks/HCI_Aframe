648edddef26228421748ea7dd2a64c5e2b8f23e2
Suuks <61313109+Suuks@users.noreply.github.com>
2021-05-15 17:55:39 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -4,6 +4,12 @@
 var rotationSpeed = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
+ function anime(){
+  targets: myOtherBox.object3D.rotation.x;
+  translateX: 270,
+  delay: anime.stagger(100) // increase delay by 100ms for each elements.
+};
+
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
